db2ba3680f38abb4b204dadb432b03a6960d55f7
geethikagrace <geethika2219@gmail.com>
2023-04-18 04:41:20 -0700

    mongodb script changes with sed

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
-Update listen address from 127.0.0.1 to 0.0.0.0
+systemctl restart mongod
