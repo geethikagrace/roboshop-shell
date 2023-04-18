2645193c597feefdd12ae284a8bcda48a0aa14c5
geethikagrace <geethika2219@gmail.com>
2023-04-18 09:33:51 -0700

    adding print statments forgot \e

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,34 @@
-echo -e "\e[32m>>>>>>>>configuring nodeJS repos<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>configuring nodeJS repos<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[32m>>>>>>>>install nodejs<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>install nodejs<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo -e "\e[32m>>>>>>>>Add application user<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>Add application user<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "\e[32m>>>>>>>>Add application directory<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>Add application directory<<<<<<<<<\e[0m"
 mkdir /app
 
-echo -e "\e[32m>>>>>>>>downloding npdejs repos<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>downloding npdejs repos<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 
-echo -e "\e[32m>>>>>>>>unzip app contents<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>unzip app contents<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
 
-echo -e "\e[32m>>>>>>>>install nodejs dependences<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>install nodejs dependences<<<<<<<<<\e[0m"
 npm install
-echo -e "\e[32m>>>>>>>>copy catalogue systemD file<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>copy catalogue systemD file<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo -e "\e[32m>>>>>>>>start catalogue service<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>start catalogue service<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
-echo -e "\e[32m>>>>>>>>copy mongodb<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>copy mongodb<<<<<<<<<\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[32m>>>>>>>>install mongodb clint<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>install mongodb clint<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-echo -e "\e[32m>>>>>>>>load schema<<<<<<<<<\[0m"
+echo -e "\e[32m>>>>>>>>load schema<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.sonydevops.online </app/schema/catalogue.js
