f7ef4adfe10c43f49cd65bbd44610069dede9098
geethikagrace <geethika2219@gmail.com>
2023-04-18 09:49:20 -0700

    changing path

--- catalogue.sh
+++ catalogue.sh
@@ -20,6 +20,7 @@ unzip /tmp/catalogue.zip
 
 echo -e "\e[32m>>>>>>>>install nodejs dependences<<<<<<<<<\e[0m"
 npm install
+
 echo -e "\e[32m>>>>>>>>copy catalogue systemD file<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
@@ -27,9 +28,12 @@ echo -e "\e[32m>>>>>>>>start catalogue service<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
+
 echo -e "\e[32m>>>>>>>>copy mongodb<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+
 echo -e "\e[32m>>>>>>>>install mongodb clint<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
 echo -e "\e[32m>>>>>>>>load schema<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.sonydevops.online </app/schema/catalogue.js
