b3e31bcde47d9876677c0967c15093df7929d79c
geethikagrace <geethika2219@gmail.com>
2023-04-18 10:33:24 -0700

    frontend

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ echo -e "\e[34m>>>>>>>>install redis<<<<<<<<<\e[0m"
 yum install redis -y
 
 echo -e "\e[34m>>>>>>>>changing conf<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[34m>>>>>>>>restating redis<<<<<<<<<\e[0m"
 systemctl enable redis
