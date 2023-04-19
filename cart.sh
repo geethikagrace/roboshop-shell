2df7ec22220e3a053daaddde38602b72ea877ed5
geethikagrace <geethika2219@gmail.com>
2023-04-19 00:39:21 -0700

    cart service

--- cart.sh
+++ cart.sh
@@ -12,20 +12,20 @@ rm -rf /app
 mkdir /app
 
 echo -e "\e[32m>>>>>>>>downloding nodejs repos<<<<<<<<<\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 
 echo -e "\e[32m>>>>>>>>unzip app contents<<<<<<<<<\e[0m"
-unzip /tmp/user.zip
+unzip /tmp/cart.zip
 
 echo -e "\e[32m>>>>>>>>install nodejs dependences<<<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[32m>>>>>>>>copy catalogue systemD file<<<<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
 echo -e "\e[32m>>>>>>>>start user service<<<<<<<<<\e[0m"
 systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+systemctl enable cart
+systemctl restart cart
 
