22a2563154845b158fa21d217690677e8c2e0984
geethikagrace <geethika2219@gmail.com>
2023-04-19 01:20:34 -0700

    rabbit mq

---
+++ rabbitmq.sh
@@ -0,0 +1,20 @@
+echo -e "\e[32m>>>>>>>>setup erlang repos<<<<<<<<<\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+echo -e "\e[32m>>>>>>>>setup rabbitmq  repos<<<<<<<<<\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "\e[32m>>>>>>>>install erlang<<<<<<<<<\e[0m"
+yum install erlang -y
+
+echo -e "\e[32m>>>>>>>>install rebbitmq<<<<<<<<<\e[0m"
+yum install rabbitmq-server -y
+
+echo -e "\e[32m>>>>>>>>start rebbitmq<<<<<<<<<\e[0m"
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
+
+echo -e "\e[32m>>>>>>>>add application user<<<<<<<<<\e[0m"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
