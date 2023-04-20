3d1225fd86dd055f2072c1482578e866a7df8e86
geethikagrace <geethika2219@gmail.com>
2023-04-20 08:42:14 -0700

    shipping dry

--- common.sh
+++ common.sh
@@ -3,17 +3,31 @@ app_user=roboshop
 
 func_print_head() {
   echo -e "\e[36m>>>>>>>> $1 <<<<<<<<<\e[0m"
+
+}
+
+func_status_check()  {
+  if [ $1 -eq 0 ]; then
+        echo -e "\e[32mSUCCESS\e[0m"
+      else
+          echo -e "\e[31mFAILURE\e[0m"
+          exit
+      fi
+
 }
   func_schema_setup() {
    if [  "$schema_setup" == "mongo"  ]; then
       func_print_head "copy mongodb repo"
       cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+      func_status_check
 
      func_print_head "install mongo clint"
      yum install mongodb-org-shell -y
+     func_status_check
 
      func_print_head "install mongodb client"
     mongo --host mongodb-dev.sonydevops.online </app/schema/${component}.js
+    func_status_check
   fi
 
 
@@ -21,14 +35,17 @@ func_print_head() {
       func_print_head "download  mavan dependences"
       mvn clean package
       mv target/shipping-1.0.jar shipping.jar
+      func_status_check
 
 
       func_print_head"install mysql client"
       yum install mysql -y
+      func_status_check
 
 
       func_print_head "load schema"
       mysql -h mysql-dev.sonydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+      func_status_check
   fi
 }
 
@@ -36,13 +53,16 @@ func_app_prereq() {
 
     func_print_head "add application user"
     useradd ${app_user}
+    func_status_check
 
     func_print_head "creat application directory"
     rm -rf /app
     mkdir /app
+    func_status_check
 
     func_print_head "download  application content"
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    func_status_check
     cd /app
 
     func_print_head "extract application content"
@@ -65,14 +85,17 @@ func_app_prereq() {
 func_nodejs() {
   func_print_head "configuring nodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_status_check
 
  func_print_head "install nodejs"
   yum install nodejs -y
+  func_status_check
 
  func_app_prereq
 
  func_print_head "install nodejs dependences"
   npm install
+  func_status_check
 
   func_schema_setup
   func_systemd_setup
@@ -84,6 +107,8 @@ func_nodejs() {
     func_print_head "install mavan"
     yum install maven -y
 
+    func_status_check
+
     func_app_prereq
 
     func_print_head"install mysql client"
@@ -91,6 +116,7 @@ func_nodejs() {
 
     func_print_head "download  mavan dependences"
     mvn clean package
+    func_status_check $?
     mv target/${component}-1.0.jar ${component}.jar
 
     func_schema_setup
