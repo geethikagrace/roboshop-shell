9b4bef4e88bf4611e784c141eace218f6642461c
geethikagrace <geethika2219@gmail.com>
2023-04-20 08:11:03 -0700

    shipping dry

--- common.sh
+++ common.sh
@@ -4,52 +4,106 @@ app_user=roboshop
 print_head() {
   echo -e "\e[36m>>>>>>>> $1 <<<<<<<<<\e[0m"
 }
-schema_setup()  {
-if [  "$schema_setup" == "mongo"  ];
-then
+  func_schema_setup() {
+   if [  "$schema_setup" == "mongo"  ]; then
+      func_print_head "copy mongodb repo"
+      cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-  echo -e "\e[32m>>>>>>>>copy mongodb<<<<<<<<<\e[0m"
-  cp ${script_path}/mongodb.repo /etc/yum.repos.d/mongo.repo
+     func_print_head "install mongo clint"
+     yum install mongodb-org-shell -y
 
-  echo -e "\e[32m>>>>>>>>install mongodb clint<<<<<<<<<\e[0m"
-  yum install mongodb-org-shell -y
+     func_print-head "install mongodb client"
+    mongo --host mongodb-dev.sonydevops.online </app/schema/${component}.js
+  fi
 
-  echo -e "\e[32m>>>>>>>>load schema<<<<<<<<<\e[0m"
-  mongo --host mongodb-dev.sonydevops.online </app/schema/${component}.js
+
+  if [ "$schema_setup" == "mysql"]; then
+      func_print_head "download  mavan dependences"
+      mvn clean package
+      mv target/shipping-1.0.jar shipping.jar
+
+
+      func_print_head"install mysql client"
+      yum install mysql -y
+
+
+      func_print_head "load schema"
+      mysql -h mysql-dev.sonydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
   fi
 }
 
+func_app_prereq() {
+
+    func_print_head "add application user"
+    useradd ${app_user}
+
+    func_print_head "creat application directory"
+    rm -rf /app
+    mkdir /app
+
+    func_print_head "download  application content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+
+    func_print_head "extract application content"
+    unzip /tmp/${component}.zip
+}
+
+   func_systemd_setup() {
+
+    func_print_head "setup systemd"
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+    func_print_head "start ${component}service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl start ${component}
+   }
+
+
+
 func_nodejs() {
-  print_head "configuring nodeJS repos"
+  func_print_head "configuring nodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
- print_head "install nodejs"
+ func_print_head "install nodejs"
   yum install nodejs -y
 
-  print_head "Add application user"
-  useradd ${app_user}
+ func_app_prereq
 
-  print_head "Add application directory"
-  rm -rf /app
-  mkdir /app
-
-  print_head "downloding nodejs repos"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-  print_head "unzip app contents"
-  unzip /tmp/${component}.zip
-
- print_head "install nodejs dependences"
+ func_print_head "install nodejs dependences"
   npm install
 
-print_head "copy catalogue systemD file"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_schema_setup
+  func_systemd_setup
 
- print_head "start catalogue service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  }
+  
+  func_java() {
 
-  schema_setup
+    func_print_head "install mavan"
+    yum install maven -y
+
+    func_app_prereq
+
+    func_print_head "download  mavan dependences"
+    mvn clean package
+    mv target/shipping-1.0.jar shipping.jar
+
+
+    func_print_head"install mysql client"
+    yum install mysql -y
+
+
+    func_print_head "load schema"
+    mysql -h mysql-dev.sonydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+
+    func_print_head "download  mavan dependences"
+    mvn clean package
+    mv target/${component}-1.0.jar ${component}.jar
+
+    func_schema_setup
+
+    func_systemd_setup
+
   }
--- shipping.sh
+++ shipping.sh
@@ -9,39 +9,8 @@ then
   exit
 fi
 
-echo -e "\e[32m>>>>>>>>install mavan<<<<<<<<<\e[0m"
-yum install maven -y
 
-echo -e "\e[32m>>>>>>>>add user<<<<<<<<<\e[0m"
-useradd ${app_user}
+component="shipping"
+schema_setup=mysql
 
-echo -e "\e[32m>>>>>>>>creat app dir<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[32m>>>>>>>>download content<<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-echo -e "\e[32m>>>>>>>>unzip<<<<<<<<<\e[0m"
-unzip /tmp/shipping.zip
-cd /app
-
-echo -e "\e[32m>>>>>>>>download dependences<<<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[32m>>>>>>>>service file<<<<<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-
-echo -e "\e[32m>>>>>>>>install mysqls<<<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[32m>>>>>>>>load shema<<<<<<<<<\e[0m"
-mysql -h mysql-dev.sonydevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[32m>>>>>>>>start service<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+func_java
