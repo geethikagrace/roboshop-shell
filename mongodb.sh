49b85a871a854ebc07a63d5401c48bb5c6879a1a
geethikagrace <geethika2219@gmail.com>
2023-04-21 01:57:05 -0700

    common

--- mongodb.sh
+++ mongodb.sh
@@ -2,8 +2,21 @@ script=$(realpath "$0")
 script_path=$(dirname  "$script")
 source ${script_path}/common.sh
 
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-systemctl enable mongod
-systemctl restart mongod
+
+
+    func_print_head "mongo repo"
+    cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>$redirect_log
+    func_status_check $?
+
+    func_print_head "install mongodb"
+    yum install mongodb-org -y &>>$redirect_log
+    func_status_check $?
+
+    func_print_head "changing default Ip address"
+    sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$redirect_log
+    func_status_check $?
+
+    func_print_head "restarting mongodb"
+    systemctl enable mongod &>>$redirect_log
+    systemctl restart mongod &>>$redirect_log
+    func_status_check $?
