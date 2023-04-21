d15027b790f4604d797ca65388a702fbd5519e0d
geethikagrace <geethika2219@gmail.com>
2023-04-20 23:23:01 -0700

    common

--- common.sh
+++ common.sh
@@ -5,9 +5,8 @@ func_print_head() {
   echo -e "\e[36m>>>>>>>> $1 <<<<<<<<<\e[0m"
 
 }
-
 func_status_check()  {
-  if [ $? -eq 0 ]; then
+  if [ $1 -eq 0 ]; then
         echo -e "\e[32mSUCCESS\e[0m"
       else
           echo -e "\e[31mFAILURE\e[0m"
@@ -19,15 +18,15 @@ func_status_check()  {
    if [  "$schema_setup" == "mongo"  ]; then
       func_print_head "copy mongodb repo"
       cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-      func_status_check
+      func_status_check $?
 
      func_print_head "install mongo clint"
      yum install mongodb-org-shell -y
-     func_status_check
+     func_status_check $?
 
      func_print_head "install mongodb client"
     mongo --host mongodb-dev.sonydevops.online </app/schema/${component}.js
-    func_status_check
+    func_status_check $?
   fi
 
 
@@ -35,17 +34,17 @@ func_status_check()  {
       func_print_head "download  mavan dependences"
       mvn clean package
       mv target/shipping-1.0.jar shipping.jar
-      func_status_check
+      func_status_check $?
 
 
       func_print_head"install mysql client"
       yum install mysql -y
-      func_status_check
+      func_status_check $?
 
 
       func_print_head "load schema"
       mysql -h mysql-dev.sonydevops.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
-      func_status_check
+      func_status_check $?
   fi
 }
 
@@ -53,31 +52,35 @@ func_app_prereq() {
 
     func_print_head "add application user"
     useradd ${app_user}
-    func_status_check
+    func_status_check $?
 
     func_print_head "creat application directory"
     rm -rf /app
     mkdir /app
-    func_status_check
+   func_status_check $?
 
     func_print_head "download  application content"
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-    func_status_check
-    cd /app
+    func_status_check $?
+
 
     func_print_head "extract application content"
+    cd /app
     unzip /tmp/${component}.zip
+    func_status_check $?
 }
 
    func_systemd_setup() {
 
     func_print_head "setup systemd"
     cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+    func_status_check $?
 
     func_print_head "start ${component}service"
     systemctl daemon-reload
     systemctl enable ${component}
     systemctl start ${component}
+    func_status_check $?
    }
 
 
@@ -85,20 +88,21 @@ func_app_prereq() {
 func_nodejs() {
   func_print_head "configuring nodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  func_status_check
+  func_status_check $?
 
   func_print_head "install nodejs"
   yum install nodejs -y
-  func_status_check
+ func_status_check $?
 
  func_app_prereq
 
  func_print_head "install nodejs dependences"
   npm install
-  func_status_check
+  func_status_check $?
 
   func_schema_setup
   func_systemd_setup
+  func_status_check $?
 
   }
   
@@ -106,13 +110,13 @@ func_nodejs() {
 
     func_print_head "install mavan"
     yum install maven -y
-
-    func_status_check
+    func_status_check $?
 
     func_app_prereq
 
     func_print_head"install mysql client"
     yum install mysql -y
+    func_status_check $?
 
     func_print_head "download  mavan dependences"
     mvn clean package
